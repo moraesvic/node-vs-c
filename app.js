b3e2c96d5fcc02c8f6c954c34ffd34e81f7e1b26
Victor Moraes <vitao.moraes@gmail.com>
2021-06-05 15:32:48 -0300

    basic functionality of nodejs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const ChildPr = require('childprocess');
 
+const app = express();
 const BIN = path.join(__dirname, 'bin/');
 
 /* https://stackoverflow.com/questions/20643470/execute-a-command-line-binary-with-node-js */
 
 app.get('/', function(req, res){
-
+  res.send('ok');
 });
 
 const PORT = process.env.PORT || 5000
