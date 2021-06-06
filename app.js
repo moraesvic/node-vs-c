776166864c5674345d959eafd60137862c803402
Victor Moraes <vitao.moraes@gmail.com>
2021-06-06 12:18:15 -0300

    final version?

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const Path = require('path');
 const TeenPr = require('teen_process');
 const Minim = require('minimist');
+const { once } = require('events');
 
 const C_BINARY = Path.join(__dirname, 'bin/binary');
 const C_TEXT = Path.join(__dirname, 'bin/text');
@@ -30,8 +31,25 @@ async function cText(size_input, print_sorted){
   strarray = strarray.split(' ');
   let numarray = strarray.map(x => Number(x));
   
-  if( print_sorted)
+  if( print_sorted )
     console.log(numarray);
+}
+
+async function asyncCText(size_input, print_sorted){
+  const proc = TeenPr.spawn(`${C_TEXT}`, [ size_input ]);
+
+  var v = [];
+  
+  proc.stdout.on('data', (data) => {
+    let strarray = data.toString().replace(' \n', '').split(' ')
+    let numarray = strarray.map(x => Number(x));
+    for(let i = 0; i < numarray.length; i++)
+      v.push(numarray[i]);
+  });
+
+  await once(proc.stdout, 'close');
+  if( print_sorted )
+    console.log(v);
 }
 
 async function cBinary(size_input, print_sorted){
@@ -43,14 +61,27 @@ async function cBinary(size_input, print_sorted){
   const buf = out.stdout;
   let v = [];
 
-  for(var i = 0; i < size_input * 4; i += 4)
+  for(var i = 0; i < buf.length; i += 4)
     v.push(buf.readInt32LE(i));
 
   if(print_sorted)
     console.log(v);
 }
 
+async function asyncCBinary(size_input, print_sorted){
+  const proc = TeenPr.spawn(`${C_BINARY}`, [ size_input ]);
 
+  let v = [];
+  
+  proc.stdout.on('data', (data) => {
+    for(let i = 0; i < data.length; i += 4)
+      v.push(data.readInt32LE(i));
+  });
+
+  await once(proc.stdout, 'close');
+  if( print_sorted )
+    console.log(v);
+}
 
 async function main(){
   const argv = Minim(process.argv.slice(2));
@@ -60,12 +91,17 @@ async function main(){
 
   console.log(`Creating and sorting random array with ${size_input} integers, from 0 to ${MAX_INT}`);
   console.log(`We are running in ${process.env.NODE_ENV || "development"} mode.\n`);
+  
   console.log('JAVASCRIPT');
   await timer(jsCreateAndSort, size_input, print_sorted);
   console.log('C-Text');
   await timer(cText, size_input, print_sorted);
+  console.log('async C-Text');
+  await timer(asyncCText, size_input, print_sorted);
   console.log('C-Binary (via buffer)');
   await timer(cBinary, size_input, print_sorted);
+  console.log('async C-Binary (via buffer)');
+  await timer(asyncCBinary, size_input, print_sorted);
 }
 
 main();
---
+++ cmd
@@ -0,0 +1,1 @@
+NODE_ENV=production node --max-old-space-size=7000 app.js -n 40000000 >> result.txt
