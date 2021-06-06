336e93597e8d0ac169bc2b471f6ba018d6322216
Victor Moraes <vitao.moraes@gmail.com>
2021-06-06 10:39:11 -0300

    now accepting command line args

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
 
-const path = require('path');
+const Path = require('path');
 const TeenPr = require('teen_process');
+const Minim = require('minimist');
 
-const C_BINARY = path.join(__dirname, 'bin/binary');
-const C_TEXT = path.join(__dirname, 'bin/text');
-
-const SIZE_INPUT = 1000;
+const C_BINARY = Path.join(__dirname, 'bin/binary');
+const C_TEXT = Path.join(__dirname, 'bin/text');
 const MAX_INT = 0x7fffffff;
-const WRITE_SORTED = false;
 
-async function timer(fn){
+async function timer(fn, size_input, print_sorted){
   const start = new Date();
-  await fn();
+  await fn(size_input, print_sorted);
   const duration = new Date() - start;
   console.log(`Code executed in ${duration} ms\n`);
 }
 
-function jsCreateAndSort(){
+function jsCreateAndSort(size_input, print_sorted){
   let v = [];
-  for(var i = 0; i < SIZE_INPUT; i++)
+  for(var i = 0; i < size_input; i++)
     v.push(Math.floor( Math.random() * MAX_INT ));
   v.sort((a,b) => a - b);
-  if(WRITE_SORTED) console.log(v);
+  if(print_sorted) console.log(v);
 }
 
-async function cBinary(){
-  const out = await TeenPr.exec(`${C_BINARY}`,
-          [ SIZE_INPUT ],
-          {encoding: 'binary'});
-
-  const buf = Buffer.from(out.stdout,'binary');
-  let v = [];
-
-  for(var i = 0; i < SIZE_INPUT * 4; i += 4)
-    v.push(buf.readInt32LE(i));
-
-  if(WRITE_SORTED)
-    console.log(v);
-}
-
-async function cText(){
+async function cText(size_input, print_sorted){
   let out = await TeenPr.exec(`${C_TEXT}`,
-          [ SIZE_INPUT ]);
+          [ size_input ]);
 
   let buf = Buffer.from(out.stdout,'binary');
 
@@ -49,18 +32,40 @@ async function cText(){
   strarray = strarray.split(' ');
   let numarray = strarray.map(x => Number(x));
   
-  if( WRITE_SORTED)
+  if( print_sorted)
     console.log(numarray);
 }
 
+async function cBinary(size_input, print_sorted){
+  const out = await TeenPr.exec(`${C_BINARY}`,
+          [ size_input ],
+          {encoding: 'binary'});
+
+  const buf = Buffer.from(out.stdout,'binary');
+  let v = [];
+
+  for(var i = 0; i < size_input * 4; i += 4)
+    v.push(buf.readInt32LE(i));
+
+  if(print_sorted)
+    console.log(v);
+}
+
+
+
 async function main(){
-  console.log(`Creating and sorting random array with ${SIZE_INPUT} integers, from 0 to ${MAX_INT}\n`);
+  const argv = Minim(process.argv.slice(2));
+
+  const size_input = argv.n || 10000;
+  const print_sorted = argv.print || false;
+
+  console.log(`Creating and sorting random array with ${size_input} integers, from 0 to ${MAX_INT}\n`);
   console.log('JAVASCRIPT');
-  await timer(jsCreateAndSort);
+  await timer(jsCreateAndSort, size_input, print_sorted);
   console.log('C-Text');
-  await timer(cText);
+  await timer(cText, size_input, print_sorted);
   console.log('C-Binary (via buffer)');
-  await timer(cBinary);
+  await timer(cBinary, size_input, print_sorted);
 }
 
 main();
