37abac2783aca285e5fa206af673ab33067aa220
Victor Moraes <talkto@moraesvic.com>
2024-01-11 17:26:24 -0300

    docs: improve documentation

--- C/shuffle_and_sort.c
+++ C/shuffle_and_sort.c
@@ -4,36 +4,54 @@
 
 /* For generating random array */
 
-void time_seed(){ srand(time(NULL)); }
-void set_seed(const int seed){ srand(seed); }
+void time_seed()
+{
+    srand(time(NULL));
+}
 
-unsigned randint_mod(const int mod){
+void set_seed(const int seed)
+{
+    srand(seed);
+}
+
+unsigned randint_mod(const int mod)
+{
     unsigned end, r;
-    if ((mod - 1) == INT_MAX) return rand();
+    if ((mod - 1) == INT_MAX) {
+        return rand();
+    }
+
     end = INT_MAX / mod;
     end *= mod;
-    while( (r = rand()) >= end);
+    do {
+        r = rand();
+    }
+    while(r >= end);
+
     return r % mod;
 }
 
-void rand_int_array(const int n, const int r_max, int v[]){
+void rand_int_array(const int n, const int r_max, int v[])
+{
     int i;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++) {
         v[i] = randint_mod(r_max);
+    }
 }
 
 /* For sorting */
 
-int separa(const int p, const int r, int v[]){
-    /* Recebe um vetor v[p..r] com p <= r. Rearranja os
-     * elementos do vetor e devolve j em p..r tal que
-     * v[p..j-1] <= v[j] < v[j+1..r]. */
+int separate(const int p, const int r, int v[]){
+    /**
+     * Receives a vector v[p..r] with p <= r. Rearranges the elements
+     * in the vector and returns j such that v[p..j-1] <= v[j] < v[j+1..r]
+     */
 
-    const int pivo = v[r];
+    const int pivot = v[r];
     int j, k, temp;
 
     for(j = p, k = p; k < r; ++k){
-        if(v[k] <= pivo){
+        if(v[k] <= pivot){
             temp = v[j];
             v[j] = v[k];
             v[k] = temp;
@@ -41,30 +59,31 @@ int separa(const int p, const int r, int v[]){
         }
     }
     v[r] = v[j];
-    v[j] = pivo;
+    v[j] = pivot;
 
     return j;
 }
 
-void quick_sort_2(const int p, const int r, int v[]){
+void quick_sort_2(const int p, const int r, int v[])
+{
     if(p >= r)
-        return;    
+        return;
     int j;
-    j = separa(p, r, v);
+    j = separate(p, r, v);
     quick_sort_2(p, j-1, v);
     quick_sort_2(j+1, r, v);
 }
 
-void quick_sort(const int n, int v[]){
+void quick_sort(const int n, int v[])
+{
     /* Wrapper */
     quick_sort_2(0, n-1, v);
 }
 
 /* API */
 
-void shuffle_and_sort(int sz, int * v, int max){
-
+void shuffle_and_sort(int sz, int * v, int max)
+{
     rand_int_array(sz, max, v);
     quick_sort(sz, v);
-
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Native NodeJS vs. C binary: a benchmark.
+
+This project was developed for learning purposes in June 2021. Please take it with
+a grain of salt.
+
+# Results
+
+The full results can be found in the file `report.html` at the root of the repository.
